8f2141a2107511c03087aebb835c96f8e1ad448c
himalaya-goswami <himalayagswm@gmail.com>
2023-06-16 03:20:54 +0530

    data-committed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,33 @@
+use ccms;
+insert into cases values (
+                             1,
+                             'a',
+                             111,
+                             'b',
+                             'first case',
+                             '06-06-2022',
+                             false,
+                             true,
+                             '06-06-2023',
+                             'nobody moves',
+                             '2022');
+
+insert into cases values (1,'a', 111,'b','first case','06-06-2022',false,true,'06-06-2023','nobody moves', '2022');
+insert into cases values (2,'a', 112,'b','first case','06-06-2022',false,true,'06-06-2023','nobody moves', '2022');
+insert into cases values (3,'a', 113,'b','first case','06-06-2022',false,true,'06-06-2023','nobody moves', '2022');
+insert into cases values (4,'a', 114,'b','first case','06-06-2022',false,true,'06-06-2023','nobody moves', '2022');
+insert into cases values (5,'a', 115,'b','first case','06-06-2022',false,true,'06-06-2023','nobody moves', '2022');
+insert into cases values (6,'a', 116,'b','first case','06-06-2022',false,true,'06-06-2023','nobody moves', '2022');
+insert into cases values (7,'a', 117,'b','first case','06-06-2022',false,true,'06-06-2023','nobody moves', '2022');
+insert into cases values (8,'a', 118,'b','first case','06-06-2022',false,true,'06-06-2023','nobody moves', '2022');
+insert into cases values (9,'a', 119,'b','first case','06-06-2022',false,true,'06-06-2023','nobody moves', '2022');
+insert into cases values (10,'a', 120,'b','first case','06-06-2022',false,true,'06-06-2023','nobody moves', '2022');
+insert into cases values (11,'a', 121,'b','first case','06-06-2022',false,true,'06-06-2023','nobody moves', '2022');
+insert into cases values (12,'a', 122,'b','first case','06-06-2022',false,true,'06-06-2023','nobody moves', '2022');
+insert into cases values (13,'a', 123,'b','first case','06-06-2022',false,true,'06-06-2023','nobody moves', '2022');
+insert into cases values (14,'a', 124,'b','first case','06-06-2022',false,true,'06-06-2023','nobody moves', '2022');
+insert into cases values (15,'a', 125,'b','first case','06-06-2022',false,true,'06-06-2023','nobody moves', '2022');
+insert into cases values (16,'a', 126,'b','first case','06-06-2022',false,true,'06-06-2023','nobody moves', '2022');
+
+select * from cases;
+describe cases;
